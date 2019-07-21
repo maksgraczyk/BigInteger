f4d19b78f98c6ee9ab3805d6ea15d6f0aa5790d2
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2019-07-21 16:20:58 +0200

    Implement bigint_convert

--- bigint.c
+++ bigint.c
@@ -1,6 +1,8 @@
 #include "bigint.h"
 #include <stdlib.h>
 
+#define MASK_8 0x00000000000000FF
+
 BigInt *bigint_new(void) {
   BigInt *number = calloc(1, sizeof(BigInt));
   return number;
@@ -12,7 +14,35 @@ void bigint_free(BigInt *number) {
 }
 
 BigInt *bigint_convert(long long number) {
-  //TODO
+  BigInt *result = bigint_new();
+
+  if (!result) {
+    return NULL;
+  }
+  
+  long long current_mask = MASK_8;
+
+  int parts_count = 0;
+
+  while ((number & current_mask) != 0) {
+    parts_count++;
+    current_mask <<= 8;
+  }
+
+  result->parts_count = parts_count;
+  result->parts = calloc(parts_count, sizeof(uint8_t));
+
+  if (!result->parts) {
+    bigint_free(result);
+    return NULL;
+  }
+
+  for (int i = 0; i < parts_count; i++) {
+    uint8_t part = (number & (MASK_8 << (8 * i))) >> (8 * i);
+    result->parts[i] = part;
+  }
+
+  return result;
 }
 
 bool bigint_add(BigInt *number, BigInt *to_add) {
