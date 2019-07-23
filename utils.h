78207f6c22d8fa3ba74f190cf5635c24733ff1d9
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2019-07-23 18:07:00 +0200

    Create utils.h with max and min functions

---
+++ utils.h
@@ -0,0 +1,12 @@
+#ifndef BIGINTEGER_UTILS_H
+#define BIGINTEGER_UTILS_H
+
+static inline int max(int a, int b) {
+  return a > b ? a : b;
+}
+
+static inline int min(int a, int b) {
+  return a < b ? a : b;
+}
+
+#endif
