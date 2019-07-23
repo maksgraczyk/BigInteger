9f488ee34b3704ee51f82c6f1255ee43f62b428a
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2019-07-23 16:52:34 +0200

    Implement bigint_lsl, bigint_lsr, bigint_asr

--- bigint.c
+++ bigint.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #define MASK_8 0xFF
+#define MASK_1 0x1
 
 BigInt *bigint_new(void) {
   BigInt *number = calloc(1, sizeof(BigInt));
@@ -50,15 +51,33 @@ bool bigint_add(BigInt *number, BigInt *to_add) {
 }
 
 void bigint_lsl(BigInt *number, int bits) {
-  //TODO
+  for (int i = 0; i < bits; i++) {
+    uint8_t carry = 0;
+    for (int j = 0; j < number->parts_count; j++) {
+      uint8_t new_part = (number->parts[j] << 1) | carry;
+      carry = number->parts[j] >> 7;
+      number->parts[j] = new_part;
+    }
+  }
+}
+
+static void _bigint_sr(BigInt *number, int bits, uint8_t initial_carry) {
+  for (int i = 0; i < bits; i++) {
+    uint8_t carry = initial_carry;
+    for (int j = number->parts_count - 1; j >= 0; j--) {
+      uint8_t new_part = (number->parts[j] >> 1) | (carry << 7);
+      carry = number->parts[j] & MASK_1;
+      number->parts[j] = new_part;
+    }
+  }
 }
 
 void bigint_lsr(BigInt *number, int bits) {
-  //TODO
+  _bigint_sr(number, bits, 0);
 }
 
 void bigint_asr(BigInt *number, int bits) {
-  //TODO
+  _bigint_sr(number, bits, number->parts[number->parts_count - 1] >> 7);
 }
 
 bool bigint_multiply(BigInt *number, long long multiply_by) {
